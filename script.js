8d0165f1c48f5671c2a615f204abbae36d5263d9
Peter M. Akasi <118350559+Akasi-M-P@users.noreply.github.com>
2023-07-20 12:40:47 +0000

    commented code

--- script.js
+++ script.js
@@ -1,34 +1,46 @@
+// Generate a random secret age between 1 and 100 and store it in the variable secret_Age
 const secret_Age = Math.trunc(Math.random() * 100) + 1;
-console.log(secret_Age);
 
+// Initialize the score to 20
 let score = 20;
 
+// Set the text content of the element with class 'number' to the secret age (for debugging purposes)
 document.querySelector('.number').textContent = secret_Age;
 
+// Add a click event listener to the element with class 'check'
 document.querySelector('.check').addEventListener('click', function () {
+  // Get the value of the user's guess from the input element with class 'guess'
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
 
+  // Check if the guess is not a valid number (empty input)
   if (!guess) {
+    // Display a message telling the user to input an age
     document.querySelector('.message').textContent = 'Please input age';
   } else if (guess === secret_Age) {
+    // If the guess is correct, display a message and do not change the score
     document.querySelector('.message').textContent = 'You guessed it RIGHT!';
-    score = score;
+    score = score; // This line is unnecessary and does not change the score
   } else if (guess > secret_Age) {
+    // If the guess is higher than the secret age
     if (score > 1) {
+      // If the score is greater than 1, display a message and decrement the score
       document.querySelector('.message').textContent = 'I am not that old';
       score--;
-      document.querySelector('.score').textContent = score;
+      document.querySelector('.score').textContent = score; // Update the displayed score
     } else {
+      // If the score is 1 or less, display a message indicating that the user lost the game and set the score to 0
       document.querySelector('.message').textContent = 'You lost the game';
       document.querySelector('.score').textContent = 0;
     }
   } else if (guess < secret_Age) {
+    // If the guess is lower than the secret age
     if (score > 1) {
+      // If the score is greater than 1, display a message and decrement the score
       document.querySelector('.message').textContent = 'I look older';
       score--;
-      document.querySelector('.score').textContent = score;
+      document.querySelector('.score').textContent = score; // Update the displayed score
     } else {
+      // If the score is 1 or less, display a message indicating that the user lost the game and set the score to 0
       document.querySelector('.message').textContent = 'You lost the game';
       document.querySelector('.score').textContent = 0;
     }
