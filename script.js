0b43ae62fd43b38235bf453b955825f3effdc9ce
Peter M. Akasi <118350559+Akasi-M-P@users.noreply.github.com>
2023-07-22 18:35:45 +0000

    did code refactoring by creating functions for duplicate DOM manipulation

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 // Generate a random secret age between 1 and 100 and store it in the variable secret_Age
-const secret_Age = Math.trunc(Math.random() * 100) + 1;
+let secret_Age = Math.trunc(Math.random() * 50) + 1;
 
 // Initialize the score to 20
 let score = 20;
 
-// Set the text content of the element with class 'number' to the secret age (for debugging purposes)
-document.querySelector('.number').textContent = secret_Age;
+let high_Score = 0;
+
+const display_Message = message => {
+  document.querySelector('.message').textContent = message;
+};
+
+const display_Number = number => {
+  document.querySelector('.number').textContent = number;
+};
+
+const display_Score = score => {
+  document.querySelector('.score').textContent = score;
+};
 
 // Add a click event listener to the element with class 'check'
 document.querySelector('.check').addEventListener('click', function () {
@@ -15,32 +26,43 @@ document.querySelector('.check').addEventListener('click', function () {
   // Check if the guess is not a valid number (empty input)
   if (!guess) {
     // Display a message telling the user to input an age
-    document.querySelector('.message').textContent = 'Please input age';
+    display_Message('Please input age');
   } else if (guess === secret_Age) {
     // If the guess is correct, display a message and do not change the score
-    document.querySelector('.message').textContent = 'You guessed it RIGHT!';
+    display_Message('You guessed it RIGHT!');
     score = score; // This line is unnecessary and does not change the score
+
+    // Set the text content of the element with class 'number' to the secret age (for debugging purposes)
+    display_Number(secret_Age);
 
     document.querySelector('body').style.backgroundColor = 'green';
 
     document.querySelector('.number').style.width = '30rem';
-  }  else if (guess !== secret_Age) {
+
+    // high_Score = score;
+
+    if (score > high_Score) {
+      high_Score = score;
+      document.querySelector('.highscore').textContent = high_Score;
+    } else {
+      high_Score = high_Score;
+    }
+  } else if (guess !== secret_Age) {
     // If the guess is higher than the secret age
     if (score > 1) {
       // If the score is greater than 1, display a message and decrement the score
-      document.querySelector('.message').textContent =
-        guess > secret_Age ? 'I am not that old' : 'I look older';
+      display_Message(
+        guess > secret_Age ? 'I am not that old' : 'I look older'
+      );
       score--;
-      document.querySelector('.score').textContent = score; // Update the displayed score
+      display_Score(score); // Update the displayed score
     } else {
       // If the score is 1 or less, display a message indicating that the user lost the game and set the score to 0
-      document.querySelector('.message').textContent = 'You lost the game';
-      document.querySelector('.score').textContent = 0;
+      display_Message('You lost the game');
+      display_Score(0);
     }
   }
 });
-
-
 
 // Resetting the entire page for new game mode
 document.querySelector('.again').addEventListener('click', function () {
@@ -51,11 +73,11 @@ document.querySelector('.again').addEventListener('click', function () {
   secret_Age = Math.trunc(Math.random() * 50) + 1;
 
   // set message to default message
-  document.querySelector('.message').textContent = 'Start guessing...';
+  display_Message('Start guessing...');
 
-  document.querySelector('.score').textContent = score;
+  display_Score(score);
 
-  document.querySelector('.number').textContent = '?';
+  display_Number('?');
 
   //set guess to default or empty value
   document.querySelector('.guess').value = '';
